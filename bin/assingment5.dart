a6bc10b8801bbb3fc6efe3f768660a79826c795c
RAKIB <rakibulislam10.net@gmail.com>
2023-12-11 23:34:50 +0600

    Assingment N1

--- bin/assingment5.dart
+++ bin/assingment5.dart
@@ -1,88 +1,31 @@
-// Define Role interface
-abstract class Role {
-  void displayRole();
-}
-
-// Define Person class implementing Role interface
-class Person implements Role {
-  String name;
-  int age;
-  String address;
-
-  Person(this.name, this.age, this.address);
-
-  @override
-  void displayRole() {
-    print('Role: Person');
-  }
-
-  String getName() {
-    return name;
-  }
-
-  int getAge() {
-    return age;
-  }
-
-  String getAddress() {
-    return address;
-  }
-}
-
-// Define Student class extending Person
-class Student extends Person {
-  int studentId;
-  List<int> courseScores;
-
-  Student(String name, int age, String address, this.studentId, this.courseScores)
-      : super(name, age, address);
-
-  @override
-  void displayRole() {
-    print('Role: Student');
-    print('Name: $name');
-    print('Age: $age');
-    print('Address: $address');
-    print('Average Score: ${calculateAverageScore().toStringAsFixed(1)}');
-  }
-
-  double calculateAverageScore() {
-    if (courseScores.isEmpty) {
-      return 0.0;
-    }
-    return courseScores.reduce((a, b) => a + b) / courseScores.length;
-  }
-}
-
-// Define Teacher class extending Person
-class Teacher extends Person {
-  String teacherId;
-  List<String> coursesTaught;
-
-  Teacher(String name, int age, String address, this.teacherId, this.coursesTaught)
-      : super(name, age, address);
-
-  @override
-  void displayRole() {
-    print('Role: Teacher');
-    print('Name: $name');
-    print('Age: $age');
-    print('Address: $address');
-    print('Courses Taught:');
-    for (var course in coursesTaught) {
-      print('- $course');
-    }
-  }
-}
-
-// Define StudentManagementSystem class with main method
 void main() {
-  // Create instances of Student and Teacher classes
-  var student = Student('Rakib', 21, '124 Main St', 123, [90, 85, 82]);
-  var teacher = Teacher('Mrs. Smith', 33, '451 Oak St', 'T123', ['Math', 'English', 'Bangla']);
+  List<Map<String, dynamic>> fruits = [
+    {"name": "Apple", "color": "Red", "price": 2.5},
+    {"name": "Banana", "color": "Yellow", "price": 1.0},
+    {"name": "Mango", "color": "Purple", "price": 3.0},
+  ];
 
-  // Display the role of each person
-  student.displayRole();
-  print('\n');
-  teacher.displayRole();
+  print("Original Fruit Details before Discount:");
+  displayFruitDetails(fruits);
+
+  applyPriceDiscount(fruits, 10);
+
+  print("\nFruit Details After Applying 10% Discount:");
+  displayFruitDetails(fruits);
 }
+
+void displayFruitDetails(List<Map<String, dynamic>> fruits) {
+  for (var fruit in fruits) {
+    print(
+        "Name: ${fruit['name']}, Color: ${fruit['color']}, Price: \$${fruit['price']}");
+  }
+}
+
+void applyPriceDiscount(List<Map<String, dynamic>> fruits, double discountPercentage) {
+  for (var fruit in fruits) {
+    double originalPrice = fruit['price'];
+    double discountAmount = (originalPrice * discountPercentage) / 100;
+    fruit['price'] = (originalPrice - discountAmount).toStringAsFixed(2);
+  }
+}
+
